571dd1b5f0f8cdd77b130457527fa1d6f9e92154
Sam Backus <sbackus@kayak.com>
2015-05-28 20:37:14 -0400

    add kayak.rb

--- game/game.rb
+++ game/game.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require './kayak'
 
 class Game < Gosu::Window
   WIDTH = 600
@@ -6,13 +7,16 @@ class Game < Gosu::Window
   def initialize
     super WIDTH, HEIGHT
     @backgroud = Gosu::Image.new("images/whitewater.jpg")
+  	@kayak = Kayak.new
   end
 
   def update
+  	@kayak.update
   end
 
   def draw
   	@backgroud.draw(0,0,-1,1.5,1.5)
+  	@kayak.draw
   end
 end
 
---
+++ game/kayak.rb
@@ -0,0 +1,23 @@
+class Kayak
+	def initialize
+		@image = Gosu::Image.new("images/kayak.png")
+		@width = @image.width
+		@height = @image.height
+		@x = Game::WIDTH/2 - @width/2
+		@y = Game::HEIGHT-@height
+		@z = 1
+		@speed = 4
+	end
+
+	def update
+		if Gosu::button_down? Gosu::KbLeft
+			@x -= @speed
+		elsif Gosu::button_down? Gosu::KbRight
+			@x += @speed
+		end
+	end
+
+	def draw
+		@image.draw(@x, @y, @z)
+	end
+end
